7bdfe793e2ecd7e67102fb54afc5ff2814bbcf11
Carlos1999uriel <carlos.uriel.teodoro@gmail.com>
2019-12-04 20:39:50 -0600

    Subiendo Proyecto en Java

---
+++ src/tarjetas/Preguntar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarjetas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author carlosu
+ */
+public class Preguntar {
+    Scanner e = new Scanner(System.in);
+   
+    public void Pregun(){
+        char op;
+        System.out.println("Tu numero esta en esta tarjeta \n");
+        
+    }
+}
